ffa8f465b29ac5bf7948121a4d0293125e14284d
Naoki Nakajima <n2.jima@gmail.com>
2019-07-15 20:45:04 +0900

    Fix html converter

--- KakaoSampleApp/app/src/main/java/io/github/pps5/kakaosampleapp/data/converter/EscapedHtmlConverter.kt
+++ KakaoSampleApp/app/src/main/java/io/github/pps5/kakaosampleapp/data/converter/EscapedHtmlConverter.kt
@@ -4,7 +4,7 @@ import com.tickaroo.tikxml.TypeConverter
 import io.github.pps5.kakaosampleapp.common.util.toHtmlSpanned
 
 class EscapedHtmlConverter : TypeConverter<String> {
-    override fun write(value: String?) = value?.toHtmlSpanned()
+    override fun write(value: String?) = value
 
-    override fun read(value: String?) = value
+    override fun read(value: String?) = value?.toHtmlSpanned()
 }
