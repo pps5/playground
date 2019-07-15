d05ebcec2b5eb0e0043787f0561f50669e7ebb38
Naoki Nakajima <n2.jima@gmail.com>
2019-07-15 15:30:44 +0900

    Merge binding adapters

--- KakaoSampleApp/app/src/main/java/io/github/pps5/kakaosampleapp/feature/newarrivals/NewArrivalsBindingAdapter.kt
+++ KakaoSampleApp/app/src/main/java/io/github/pps5/kakaosampleapp/feature/newarrivals/NewArrivalsBindingAdapter.kt
@@ -1,10 +1,20 @@
-package io.github.pps5.kakaosampleapp.feature.newarrivals.bindingadapter
+package io.github.pps5.kakaosampleapp.feature.newarrivals
 
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import io.github.pps5.kakaosampleapp.common.vo.Resource
 import io.github.pps5.kakaosampleapp.data.entity.Entry
-import io.github.pps5.kakaosampleapp.feature.newarrivals.NewArrivalsAdapter
+import org.threeten.bp.ZonedDateTime
+import org.threeten.bp.format.DateTimeFormatter
+
+private const val PUBLISHED_DATE_TIME_FORMAT = "yyyy年M月d日"
+private val formatter = DateTimeFormatter.ofPattern(PUBLISHED_DATE_TIME_FORMAT)
+
+@BindingAdapter(value = ["bind:published"])
+fun TextView.setPublished(published: ZonedDateTime) {
+    this.text = formatter.format(published)
+}
 
 @BindingAdapter(value = ["bind:newArrivals"])
 fun RecyclerView.setNewArrivals(newArrivals: Resource<List<Entry>>?) {
